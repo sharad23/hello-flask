ee2c2dcf2b78fc5539d9be9dc8e014a094876570
Sharad Baidya <sharadbaidya@SPMBP064.local>
2019-09-17 20:23:53 +0545

    some more change

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, render_template      
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
